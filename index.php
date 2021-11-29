46b93fb330555eb786374a68b3fc12122d826a1b
Saho Alex <gmsaho@gmail.com>
2021-11-29 17:40:45 +0000

    final v1)

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
-<?
+<?php
+
+require "db.php";
+require "functions.php";
 echo "TEST";
+//echo $db;
+$q =  $_GET['q'];
+
+$r = explode("/",$q);
+//print_r($r);
+//
+
+switch($r[0]) {
+	case "user":
+		get_user_info($link,$r[1]);
+		break;
+	case "create":
+		create_user($link,$r[1],$r[2],$r[3]);
+		break;
+	case "delete":
+		delete_user($link,$r[1]);
+		break;
+	case "update":
+		update_user($link,$r[1],$r[2],$r[3]);
+		break;
+	case "auth":
+		auth_user($link,$r[1],$r[2]);
+		break;
+	default:
+		die("Error reques");
+}
+
+
+
+
+
+
+
 
