10d91f3b453b2ae17aea96a36c4dc7f053f56d8a
Saho Alex <gmsaho@gmail.com>
2021-11-30 04:58:41 +0000

    add auth for requests

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 /// funcs
+function isAuth($link){
+	if(isset($_SERVER['HTTP_TOKEN'])) {
+		$token = $_SERVER['HTTP_TOKEN'];
+		$data = mysqli_query($link,"SELECT * FROM `token` WHERE `token` = '$token'");
+		if(mysqli_num_rows($data)>0) return true;
+	}
+	return false;
+}
+
 function get_user_info($link,$name){
 	echo "name: ".$name."<br>";
 	$data = mysqli_query($link,"SELECT * FROM `users` WHERE `name` = '$name'");
@@ -39,10 +48,20 @@ function auth_user($link,$name,$pass){
 	$data = mysqli_query($link,"SELECT `password` FROM `users` WHERE `name` = '$name'");
 	if(!$data) die(mysqli_error($link));
 	$data = mysqli_fetch_assoc($data);
-	if($data["password"] == $pass) 	
-		 $r = [ "auth" => "authorized" ];
-	else
+	if($data["password"] == $pass) {	
+		$r = [ "auth" => "authorized" ];
+		$a_token = md5(now()+'sdfsdfgsdfsdf'+$name);
+		$timestamp = time();
+		$data = mysqli_query($link,"INSERT INTO `token` (`token`,`expdate`) VALUES ('$a_token','$timestamp'");
+		if(!$data) die(mysqli_error($link));
+
+	}
+	else 
+	{
 		$r = [ "auth" => "auth_denied" ];
+//		header('HTTP/1.1 401 Unauthorized');
+		http_response_code(401);
+	}
 	echo json_encode($r);
 
 }
--- index.php
+++ index.php
@@ -4,29 +4,33 @@ require "db.php";
 require "functions.php";
 echo "TEST";
 //echo $db;
+$auth = false;
 $q =  $_GET['q'];
-
 $r = explode("/",$q);
 //print_r($r);
+if($r[0]=='auth') {
+	auth_user($link);
+ }
 
-switch($r[0]) {
-	case "user":
-		get_user_info($link,$r[1]);
-		break;
-	case "create":
-		create_user($link,$r[1],$r[2],$r[3]);
-		break;
-	case "delete":
-		delete_user($link,$r[1]);
-		break;
-	case "update":
-		update_user($link,$r[1],$r[2],$r[3]);
-		break;
-	case "auth":
-		auth_user($link,$r[1],$r[2]);
-		break;
+$auth = isAuth($link);
+if($auth) {
+	switch($r[0]) {
+		case "user":
+			(isAuth($authok)) get_user_info($link,$r[1]);
+			break;
+		case "create":
+
+			create_user($link,$r[1],$r[2],$r[3]);
+			break;
+		case "delete":
+			delete_user($link,$r[1]);
+			break;
+		case "update":
+			update_user($link,$r[1],$r[2],$r[3]);
+			break;
 	default:
-		die("Error reques");
+			die("Error reques");
+		}
 }
 
 
